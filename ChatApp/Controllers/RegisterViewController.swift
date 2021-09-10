81ca9c1f9e36ec9400586fe5378dbf71a9aecbbf
Ahmed <ahmed.sharf.777@gmail.com>
2021-09-11 01:19:30 +0200

    install Firebase pod. configure Firebase.

--- ChatApp/Controllers/RegisterViewController.swift
+++ ChatApp/Controllers/RegisterViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
+    }
+    
+    
+    @IBAction func registerPressed(_ sender: UIButton) {
     }
 }
