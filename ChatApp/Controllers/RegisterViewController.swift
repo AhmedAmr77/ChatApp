a64666e9b7c307ead0a9772d11f3dae5f3785673
Ahmed <ahmed.sharf.777@gmail.com>
2021-09-10 23:29:53 +0200

    finish register screen UI.

---
+++ ChatApp/Controllers/RegisterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterViewController.swift
+//  ChatApp
+//
+//  Created by Ahmd Amr on 10/09/2021.
+//  Copyright © 2021 ahmdamr. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
