c5164e01674fbdfb2d700051f38d53e0077a8005
Ahmed <ahmed.sharf.777@gmail.com>
2021-09-20 00:42:25 +0200

    finish chat screen UI.

---
+++ ChatApp/Controllers/ChatViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChatViewController.swift
+//  ChatApp
+//
+//  Created by Ahmd Amr on 20/09/2021.
+//  Copyright © 2021 ahmdamr. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+     }
+    
+}
